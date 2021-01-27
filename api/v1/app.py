f9252f52314ab679b0ba36b3d93a236fa52a7913
DamnDanielV <1821@holbertonschool.com>
2021-01-27 17:20:55 -0500

    fixed pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,21 +13,26 @@ app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 # register the blueprint app_views to your Flask instance app
 app.register_blueprint(app_views, url="/app_views")
 
-# declare a method to handle @app.teardown_appcontext that calls storage.close()
+
+# declare a method to handle @app.teardown_appcontext that calls storage.close
 @app.teardown_appcontext
-def teardown(exception):
+def tear(exception):
     """calls storage.close"""
     storage.close()
 
+
+# create a handler for 404 errors that returns a JSON-formatted
+# 404 status code response. The content should be: "error": "Not found"
 @app.errorhandler(404)
-def abort(e):
-    """ returns 404 template on 404 code status """
+def error_han(e):
+    """ 404 errors that returns a JSON """
     return jsonify({"error": "Not found"}), 404
+
 
 if __name__ == "__main__":
     host = environ.get('HBNB_API_HOST', '0.0.0.0')
     port = int(environ.get('HBNB_API_PORT', 5000))
-    app.register_error_handler(404, abort)
+    app.register_error_handler(404, error_han)
     app.run(
         host=host,
         port=port,
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,11 +6,13 @@ from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
+
 # create a route /status on the object app_views
 # that returns a JSON: "status": "OK"
 @app_views.route("/status", strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
 
 # Create an endpoint that retrieves the number of each objects by type
 @app_views.route("/stats", strict_slashes=False)
