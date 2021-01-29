d7a4bb8103b19509fa1ad2ca422a32d7637829eb
DamnDanielV <1821@holbertonschool.com>
2021-01-28 19:16:05 -0500

    fixed outputs

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -11,29 +11,57 @@ from api.v1.views import app_views
 
 
 @app_views.route("/amenities",
-                 methods=["GET", "POST"],
+                 methods=["GET"],
                  strict_slashes=False)
-def get_post_amenity():
-    """list amenities from a state and creates a new city"""
-    if request.method == "POST":
-        amenity_json = request.get_json(silent=True)
-        if amenity_json is None:
-            abort(400, "Not a JSON")
+def get_amenity():
+    """get an amenity"""
+    amenities = storage.all(Amenity)
+    l_amenities = [amenity.to_dict() for amenity in amenities.values()]
+    return jsonify(l_amenities)
 
-        if "name" not in request.json:
-            abort(400, "Missing name")
 
-        n_amenity = Amenity(**amenity_json)
-        storage.new(n_amenity)
-        storage.save()
-        storage.close()
-        return jsonify(n_amenity.to_dict()), 201
+@app_views.route("/amenities",
+                 methods=["POST"],
+                 strict_slashes=False)
+def post_amenity():
+    """POST method"""
+    amenity_json = request.get_json(silent=True)
+    if amenity_json is None:
+        abort(400, "Not a JSON")
+
+    if "name" not in request.json:
+        abort(400, "Missing name")
+
+    n_amenity = Amenity(**amenity_json)
+    n_amenity.save()
+    return jsonify(n_amenity.to_dict()), 201
+
+
+
+# @app_views.route("/amenities",
+#                  methods=["GET", "POST"],
+#                  strict_slashes=False)
+# def get_post_amenity():
+#     """list amenities from a state and creates a new city"""
+#     if request.method == "POST":
+#         amenity_json = request.get_json(silent=True)
+#         if amenity_json is None:
+#             abort(400, "Not a JSON")
+
+#         if "name" not in request.json:
+#             abort(400, "Missing name")
+
+#         n_amenity = Amenity(**amenity_json)
+#         storage.new(n_amenity)
+#         storage.save()
+#         storage.close()
+#         return jsonify(n_amenity.to_dict()), 201
     
-    elif request.method == "GET":
+#     elif request.method == "GET":
 
-        amenities = storage.all(Amenity)
-        l_amenities = [amenity.to_dict() for amenity in amenities.values()]
-        return jsonify(l_amenities)
+#         amenities = storage.all(Amenity)
+#         l_amenities = [amenity.to_dict() for amenity in amenities.values()]
+#         return jsonify(l_amenities)
 
 
 @app_views.route("/amenities/<amenity_id>",
